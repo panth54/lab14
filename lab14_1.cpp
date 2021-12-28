bcce3ba1fec9e4ff763a246e1c2e35a9838cc0b0
Phanthat Muenprap <phanthat54@gmail.com>
2021-12-28 11:30:02 +0700

    lab14_1 finish

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,28 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double AV, SD = 0, GM = 1, HM = 0, sum = 0, max = A[0], min = A[0];
+
+    for (int i=0; i<N; ++i){
+        sum += A[i];
+        SD += pow(A[i], 2);
+        GM *= A[i];
+        HM += 1/A[i];
+        if (A[i] > max) max = A[i];
+        if (A[i] < min) min = A[i];
+    }
+
+    AV = sum / N;
+    SD = sqrt((SD/N) - pow(AV, 2));
+    GM = pow(GM, 1.0/N);
+    HM = N / HM;
+    
+    B[0] = AV;
+    B[1] = SD;
+    B[2] = GM;
+    B[3] = HM;
+    B[4] = max;
+    B[5] = min;
+}
